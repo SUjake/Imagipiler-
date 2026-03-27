6f2270444d5f59052005eacaeadba8fc0437def8
rohanjoshi <joshirohan2005@gmail.com>
2026-03-27 20:12:42 +0530

    modify algorithm

--- algorithm.py
+++ algorithm.py
@@ -15,3 +15,58 @@ class IRGenerator:
     def generate(self, node):
         if node is None:
             return None
+# ----- BASIC VALUES ----------------------
+        if node.type == "num":
+            return str(node.value)
+
+        if node.type == "str":
+            return f'"{node.value}"'
+
+        if node.type == "id":
+            return node.value
+# ----- ARITHMETIC ------------------------
+        if node.type in ["+", "-", "*", "/", "<", ">", "<=", ">=", "==", "!="]:
+            left = self.generate(node.children[0])
+            right = self.generate(node.children[1])
+            temp = self.new_temp()
+            self.code.append(f"{temp} = {left} {node.type} {right}")
+            return temp
+# ----- IF STATEMENT -----
+        if node.type == "if":
+            cond = self.generate(node.children[0])
+            else_label = self.new_label()
+            end_label = self.new_label()
+
+            self.code.append(f"if {cond} False goto {else_label}")
+            for stmt in node.children[1].children:
+                self.generate(stmt)
+
+            self.code.append(f"goto {end_label}")
+            self.code.append(f"{else_label}:")
+            self.code.append(f"{end_label}:")
+# ----- IF-ELSE STATEMENT -----
+        if node.type == "ifelse":
+            cond = self.generate(node.children[0])
+            else_label = self.new_label()
+            end_label = self.new_label()
+
+            self.code.append(f"if {cond} False goto {else_label}")
+
+            # then block
+            for stmt in node.children[1].children:
+                self.generate(stmt)
+
+            self.code.append(f"goto {end_label}")
+
+            # else block
+            self.code.append(f"{else_label}:")
+            for stmt in node.children[2].children:
+                self.generate(stmt)
+            self.code.append(f"{end_label}:")
+# ----- ASSIGNMENT -------------------------
+        if node.type == "=":
+            var = node.children[0].value
+            val = self.generate(node.children[1])
+            self.code.append(f"{var} = {val}")
+            return var
+
