1ac97f0986d215b4cf954c40ef9620544f125c7e
Aditya Agarwal <adityaagrawal6060@gmail.com>
2026-03-27 15:00:00 +0530

    Added token parsing loop

--- visualiser.py
+++ visualiser.py
@@ -4,3 +4,13 @@ from html import escape
 
 def build_token_table(tokens):
     rows = ""
+    for tok in tokens:
+        
+        rows += f"""
+        <TR>
+            <TD>{escape(str(tok.type))}</TD>
+            <TD>{escape(str(tok.value))}</TD>
+            <TD>{tok.lineno}</TD>
+            <TD>{tok.lexpos}</TD>
+        </TR>
+        """
